1416f01fb298e0fd265c1cbb3846011daa4632a9
chancity <hawbakerchancey@gmail.com>
2019-08-23 21:46:35 -0700

    Header Fix for Kin Blockchain
    fix this before someone starts using this SDK

--- stellar-dotnet-sdk/responses/Response.cs
+++ stellar-dotnet-sdk/responses/Response.cs
@@ -7,17 +7,26 @@ namespace stellar_dotnet_sdk.responses
     [JsonObject(MemberSerialization.OptIn)]
     public abstract class Response
     {
-        protected int RateLimitLimit { get; private set; }
+        protected int RateLimitLimit { get; private set; } = 101;
 
-        protected int RateLimitRemaining { get; private set; }
+        protected int RateLimitRemaining { get; private set; } = 100;
 
-        protected int RateLimitReset { get; private set; }
+        protected int RateLimitReset { get; private set; } = 1;
 
         public void SetHeaders(HttpResponseHeaders headers)
         {
-            RateLimitLimit = int.Parse(headers.GetValues("X-Ratelimit-Limit").First());
-            RateLimitRemaining = int.Parse(headers.GetValues("X-Ratelimit-Remaining").First());
-            RateLimitReset = int.Parse(headers.GetValues("X-Ratelimit-Reset").First());
+            if (headers.TryGetValues("X-Ratelimit-Limit", out var rateLimit))
+            {
+                RateLimitLimit = int.Parse(rateLimit.FirstOrDefault() ?? "101");
+            }
+            if (headers.TryGetValues("X-Ratelimit-Remaining", out var rateLimitRemaining))
+            {
+                RateLimitRemaining = int.Parse(rateLimitRemaining.FirstOrDefault() ?? "100");
+            }
+            if (headers.TryGetValues("X-Ratelimit-Reset", out var rateLimitReset))
+            {
+                RateLimitReset = int.Parse(rateLimitReset.FirstOrDefault() ?? "1");
+            }
         }
     }
 }
